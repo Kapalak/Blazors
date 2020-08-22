d48a9c6b97b69a4edf85ed849c244a2d4cabb1b6
Aymen BEN MILED <benmiledaymen@hotmail.com>
2020-08-22 08:36:44 +0100

    init kapalak mobile app : dotnet new blazormobile

---
+++ mobil/kapalak/kapalak/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BlazorMobile.Common;
+using BlazorMobile.Components;
+using kapalak.Helpers;
+using BlazorMobile.Services;
+
+namespace kapalak
+{
+	public partial class App : BlazorApplication
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            ServiceRegistrationHelper.RegisterServices();
+
+            WebApplicationFactory.SetHttpPort(8888);
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ mobil/kapalak/kapalak/Services/XamarinBridge.cs
@@ -0,0 +1,27 @@
+﻿using BlazorMobile.Common;
+using kapalak.Common.Interfaces;
+using kapalak.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace kapalak.Services
+{
+    public class XamarinBridge : IXamarinBridge
+    {
+        public async Task<List<string>> DisplayAlert(string title, string msg, string cancel)
+        {
+            await App.Current.MainPage.DisplayAlert(title, msg, cancel);
+
+            List<string> result = new List<string>()
+            {
+                "Lorem",
+                "Ipsum",
+                "Dolorem",
+            };
+
+            return result;
+        }
+    }
+}
